07b64ebbe861a9a0b2fd8827fbd2d7ee929ee66a
Andrew Dassonville <andrewda@users.noreply.github.com>
2015-07-25 10:38:02 -0700

    Changed `gamesAttr`

--- index.js
+++ index.js
@@ -1,24 +1,37 @@
 var request = require("request");
 var parseString = require('xml2js').parseString;
 
-module.exports.getUpcoming = function(fn) {
+var upcomingGames = {
+  _HLTV_URL: "http://www.hltv.org/hltv.rss.php?pri=15"
+};
+
+upcomingGames.getUpcoming = function(callback) {
   var gamesAttr = {};
-  request({
-    uri: "http://www.hltv.org/hltv.rss.php?pri=15"
-  }, function(error, response, body) {
+  request({ uri: upcomingGames._HLTV_URL }, function(error, response, body) {
     parseString(body, function(err, result) {
-      if (err) {
-        console.log(err);
-      }
+      if (err) throw err;
       
       gamesAttr = {
         'upcomingTotal': result.rss.channel[0].item.length,
-        'games': []
+        'games': {}
       };
+      
       for (var i = 0; i < gamesAttr.upcomingTotal; i++) {
-        gamesAttr.games.push(result.rss.channel[0].item[i]);
+        var matchObj = {
+          team1 : result.rss.channel[0].item[i].title[0].split(" vs ")[0],
+          team2 : result.rss.channel[0].item[i].title[0].split(" vs ")[1],
+          title : result.rss.channel[0].item[i].title[0],
+          link  : result.rss.channel[0].item[i].link[0],
+          event : result.rss.channel[0].item[i].description[0],
+          date  : result.rss.channel[0].item[i].pubDate[0]
+        };
+        
+        gamesAttr.games[matchObj.link.match(/\/match\/([0-9]+)/, '')[1]] = matchObj;
       }
-      fn(gamesAttr);
+      
+      callback(gamesAttr);
     });
   });
 };
+
+module.exports = upcomingGames;
